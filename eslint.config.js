bd14d00ac66c152f1f0f53c37d3607535bd176fc
palmdrop <anton@exlex.se>
2024-11-23 11:35:37 +0100

    initial solid start setup with eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts}']
+  },
+  {
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      'no-unused-vars': 'error',
+      indent: ['error', 2],
+      'no-multi-spaces': 'error'
+    }
+  },
+  eslintConfigPrettier
+];
---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import './styles/reset.css';
+import './styles/fonts.css';
+import './styles/global.css';
+
+export default function App() {
+  return (
+    <main>test</main>
+  );
+}
